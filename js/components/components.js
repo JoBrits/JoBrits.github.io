0e8d3f551badb984151b993700b0395678dcdd7e
Jo <JBrits@fnb.co.za>
2019-04-24 16:47:04 +0200

    Latest Updates

---
+++ js/components/components.js
@@ -0,0 +1,18 @@
+
+// alert("components");
+
+$(document).ready(function(){
+  
+  $('#ds__main__nav').load("../symbols/base/navigation/ds-menu.html");
+  
+});
+
+
+
+// 1 level deeper;
+
+$(document).ready(function(){
+  
+  $('#ds__main__nav').load("../../symbols/base/navigation/ds-menu.html");
+  
+});
