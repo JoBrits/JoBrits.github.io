fc8aac3a9df7a4e14457c49ade1718ef8dda6555
Jo <JBrits@fnb.co.za>
2019-05-03 12:40:35 +0200

    Fixes

--- js/base/navigation/side-menu-ds.js
+++ js/base/navigation/side-menu-ds.js
@@ -11,18 +11,20 @@ function menuTriger() {
     $('.ds__main__nav__menu__icon').addClass(' ds__main__nav__menu__icon--active');
     $('.ds__main__nav__links').addClass(' ds__main__nav__links--active');
     $('.ds__main__content').addClass(' ds__main__content--menuOpen');
+    $('.ds__main__cookies').addClass(' ds__main__cookies--menuOpen');
     menuBtn = true;
 
   }  else {
     $('.ds__main__nav__menu__icon').removeClass(' ds__main__nav__menu__icon--active');
     $('.ds__main__nav__links').removeClass(' ds__main__nav__links--active');
     $('.ds__main__content').removeClass(' ds__main__content--menuOpen');
+    $('.ds__main__cookies').removeClass(' ds__main__cookies--menuOpen');
     menuBtn = false;
   } 
 
 };
 
-// Nave Menu
+// Nav Menu
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
     current = $(this);
---
+++ js/cards/overlay-card.js
@@ -0,0 +1,6 @@
+function openOverlay() {
+  $('.overlay-card').addClass(' overlay-card--active');
+  $('.overlay-card__side--card').addClass(' overlay-card__side--card--in-active');
+  $('.overlay-card__side--overlay').addClass(' overlay-card__side--overlay--active');
+}
+
